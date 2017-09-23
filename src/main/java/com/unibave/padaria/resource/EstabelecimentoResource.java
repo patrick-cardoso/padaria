f511b437d6911ffdb36e065a5b1db8b038a04cab
Patrick da Silva Cardoso <patrick.cardoso@yahoo.com.br>
2017-09-23 11:13:02 -0300

    Documentação api Estabelecimentos

--- src/main/java/com/unibave/padaria/resource/EstabelecimentoResource.java
+++ src/main/java/com/unibave/padaria/resource/EstabelecimentoResource.java
@@ -1,6 +1,10 @@
 package com.unibave.padaria.resource;
 
 import com.unibave.padaria.model.Estabelecimento;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import javax.validation.Valid;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -15,22 +19,46 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+@Api(value = "Cadastro de estabelecimentos", tags = "produtores",
+        consumes = MediaType.APPLICATION_JSON, produces = MediaType.APPLICATION_JSON)
 @Path("/estabelecimentos")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public interface EstabelecimentoResource {
 
+    @ApiOperation(value = "Adiciona estabelecimento")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Produtor adicionado com sucesso", response = Produtor.class)
+    })
     @POST
     Response adiciona(@Valid final Estabelecimento aluno);
 
+    @ApiOperation(value = "Atualiza estabelecimento")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Estabelecimento atualizado com sucesso", response = Produtor.class)
+        ,
+        @ApiResponse(code = 404, message = "Estabelecimento não encontrado")
+    })
     @PUT
     @Path(value = "{id}")
     Response atualiza(@PathParam(value = "id") final Long id, @Valid final Estabelecimento estabelecimeto);
 
+    @ApiOperation(value = "Exclui estabelecimento")
+    @ApiResponses(value = {
+        @ApiResponse(code = 204, message = "Estabelecimento excluído com sucesso")
+        ,
+        @ApiResponse(code = 404, message = "Estabelecimento não encontrado")
+    })
     @DELETE
     @Path(value = "{id}")
     Response deleta(@PathParam(value = "id") final Long id);
 
+    @ApiOperation(value = "Busca estabelecimento")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Estabelec retornado com sucesso", response = Produtor.class)
+        ,
+        @ApiResponse(code = 404, message = "Produtor não encontrado")
+    })
     @GET
     @Path(value = "{id}")
     Response busca(@PathParam(value = "id") final Long id);
