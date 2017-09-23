16d78be2a1f481a9007156cecc20f9dd67ed9899
Patrick da Silva Cardoso <patrick.cardoso@yahoo.com.br>
2017-09-23 09:38:57 -0300

    Estrutura inicial

---
+++ src/test/java/com/unibave/padaria/padaria/PadariaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.unibave.padaria.padaria;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PadariaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
